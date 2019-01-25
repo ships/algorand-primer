c54679b2ede3e80bd7da0f621e7cbca354b01973
early evening <eve@haut-kultur.org>
2019-01-25 13:50:29 -0800

    add link to qr code to survey

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import BgImage from './image'
 import TeX from './jaxtex'
 import CaptionFloat from './caption'
 import Layout from './layout'
+import QRLink from './qrlink'
 
-export { BgImage, TeX, CaptionFloat, Layout }
+export { BgImage, TeX, CaptionFloat, Layout, QRLink }
